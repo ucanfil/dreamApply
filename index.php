036424a8632f424967246b1be2c597fc8429c257
Burak Tilek <burak.tilek@gmail.com>
2019-02-09 17:40:28 +0300

    English languages names added to the output using DomDocument and XPath

--- index.php
+++ index.php
@@ -1,37 +1,44 @@
 <?php
 
-  // $ch = curl_init();
-  // $url = 'https://unicode.org/repos/cldr/trunk/common/supplemental/supplementalData.xml';
-  // curl_setopt( $ch, CURLOPT_URL, $url);
-  // curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
-
-  // $content = curl_exec($ch);
-  // $xml=simplexml_load_string($content);
- 
-  // var_dump($xml);
-
-  
   $xml = simplexml_load_file('https://unicode.org/repos/cldr/trunk/common/supplemental/supplementalData.xml');
 
-  foreach ($xml->territoryInfo->territory as $row) {
-    $population = $row->attributes()['population'];
-    $type = isset($row->languagePopulation) ? '' . $row->languagePopulation->attributes()['type'] : null;
-    $populationPercent = isset($row->languagePopulation) ? $row->languagePopulation->attributes()['populationPercent'] : null;
-    $totalPerson = round($populationPercent * $population * 0.01);
-    $lang['type'] = $type;
-    $lang['totalPerson'] = $totalPerson;
-    $data[] = $lang;
+  $file = 'https://unicode.org/repos/cldr/trunk/common/supplemental/supplementalData.xml';
+  $doc = new DOMDocument();
+  $doc->load($file);
+  $xp = new DOMXPath($doc);
+
+  $comments = $xp->evaluate('/supplementalData/territoryInfo/territory/languagePopulation/following::comment()[1]');
+
+  $names = Array();
+
+  foreach ($comments as $comment) {
+    $names[] = (string)$comment->data;
+  }
+
+  foreach ($xml->territoryInfo->territory as $rows) {
+    $population = $rows->attributes()['population'];
+    foreach ($rows as $languagePopulation) {
+      $type = isset($languagePopulation) ? '' . $languagePopulation->attributes()['type'] : null;
+      $populationPercent = isset($languagePopulation) ? $languagePopulation->attributes()['populationPercent'] : null;
+      $totalPerson = round($populationPercent * $population * 0.01);
+      $lang['type'] = $type;
+      $lang['totalPerson'] = $totalPerson;
+      $lang['langNames'] = $names[0] ?? null;
+      $names = array_slice($names, 1);
+      $data[] = $lang;
+    }
   }
 
   foreach ($data as $d) {
     if (empty($tully[$d['type']])) {
-      $tully[$d['type']] = $d['totalPerson'];
+      $tully[$d['type']] = [$d['totalPerson'], $d['langNames']];
     } else {
-      $tully[$d['type']] += $d['totalPerson'];
+      $total = $d['totalPerson'] + $tully[$d['type']][0];
+      $tully[$d['type']] = [$total, $d['langNames']];
     }
   }
 
   arsort($tully);
   foreach ($tully as $key => $value) {
-    echo $key . ' ' . $value . "\n";
+    echo $key . ' ' . $value[1] . ' ' . $value[0] . "\n";
   }
